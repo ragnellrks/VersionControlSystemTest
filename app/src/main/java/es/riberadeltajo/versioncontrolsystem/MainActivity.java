95a0e5785225cced943201af82b2bcfdfe088b21
Ragnell <ragnellrks@gmail.com>
2023-10-19 19:17:08 +0200

    Commit test from Android Studio

---
+++ app/src/main/java/es/riberadeltajo/versioncontrolsystem/MainActivity.java
@@ -0,0 +1,14 @@
+package es.riberadeltajo.versioncontrolsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
